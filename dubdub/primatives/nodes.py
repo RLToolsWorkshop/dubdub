700df90139c900c6642fe3ed06d3a38b7c0939c0
Kevin Hill <kah.kevin.hill@gmail.com>
2021-07-03 06:06:23 -0400

    feat(scanner): Finished slightly broken scanner.
    The scanner is largely finished. However, there are some serious
    indexing issues with it that need to be squashed out. Pushing to this
    branch now instead of waiting to fix it. Will Fix it in a branch.

--- dubdub/primatives/nodes.py
+++ dubdub/primatives/nodes.py
@@ -1,22 +1,24 @@
 from typing import Any, AnyStr
 
 from dubdub import dataclass
-from dubdub.primatives.types import TokenTypes
+from dubdub.primatives.types import TokenType
 
 
 @dataclass
 class Token:
-    token_type: TokenTypes
+    token_type: TokenType
     lexeme: str
-    literal: AnyStr
+    literal: Any
     line: int
-    
+
     def __str__(self) -> str:
         return f"Token(type={self.token_type}, lexeme={self.lexeme}, literal={self.literal})"
 
+
 def main():
-    sample = Token(token_type=TokenTypes.AND, lexeme="and", literal="and", line=1)
+    sample = Token(token_type=TokenType.AND, lexeme="and", literal="and", line=1)
     print(sample)
+
 
 if __name__ == "__main__":
     main()
